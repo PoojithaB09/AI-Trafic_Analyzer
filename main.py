ca2c01819d40883d067abfd0de2f530f03947ad6
itsamrithaa <itsamrithaa@gmail.com>
2025-02-22 12:02:11 -0500

    Import packages

---
+++ main.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+import torch
+import torchvision
+import ultralytics
+import pandas as pd
+import matplotlib.pyplot as plt
+
+print("All packages imported successfully!")
